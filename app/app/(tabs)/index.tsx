8f25f82ca6defddfc0af776bcde4efa50f9da7d3
brkss <berkassebrahim@gmail.com>
2025-07-09 22:15:10 +0100

    👋 inital commit : setup expo app

---
+++ app/app/(tabs)/index.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.currentReadingContainer}>
+          <Text style={styles.currentReadingValue}>155</Text>
+          <Text style={styles.currentReadingUnit}>mg/dL</Text>
+        </View>
+        <Text style={styles.readingTime}>last read 18:05</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  currentReadingContainer: {
+    marginTop: 10,
+    flexDirection: "row",
+    alignItems: "baseline",
+    justifyContent: "flex-start",
+  },
+  currentReadingValue: {
+    fontSize: 70,
+    fontWeight: 500,
+  },
+  currentReadingUnit: {
+    fontSize: 25,
+    marginLeft: 4,
+  },
+  readingTime: {
+    textAlign: "left",
+    opacity: 0.8,
+  },
+});
