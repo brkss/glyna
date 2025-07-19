259dd44da374ce0281e786c9a2f637ccd78cbab4
brkss <berkassebrahim@gmail.com>
2025-07-19 16:11:04 +0100

    👋 intialize expo app

---
+++ app/app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Tabs } from "expo-router";
+
+const TabLayout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="record"
+        options={{ title: "Record", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
